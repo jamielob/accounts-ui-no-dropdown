9be5284bc3d5691fa92193d718db661bdddd546b
Jamie Loberman <jamie@beautifulcoding.com>
2015-03-25 18:31:28 -0700

    Removed template rendered

--- accounts-ui-no-dropdown.js
+++ accounts-ui-no-dropdown.js
@@ -1,8 +1,8 @@
 if (Meteor.isClient) {
-  Template.loginButtons.rendered = function() {
+  
     Deps.autorun(function(){
     	Accounts._loginButtonsSession.get('dropdownVisible');
     	Accounts._loginButtonsSession.set('dropdownVisible', true);	
 	});
-  }
+  
 }
